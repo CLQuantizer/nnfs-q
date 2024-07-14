17e59041bf84ce0bb3f6f0af95bd4fde5b89e0dd
ezio <ezio.xie@binance.com>
2024-07-14 14:11:08 +0100

    go

---
+++ classes.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+class LayerDense:
+    def __init__(self, n_inputs, n_neurons):
+        self.output = None
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+class ActivationReLU:
+    def __init__(self):
+        self.output = None
+
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+class ActivationSoftmax:
+    def __init__(self):
+        self.output = None
+
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+    
+class LossCategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples_length = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+        correct_confidences = None
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples_length), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
